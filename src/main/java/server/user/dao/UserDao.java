3ff75da8b97d2b96b04a1bae50efb6ffceb4335a
qhtkdrma <kimwonkyu15@gmail.com>
2022-02-14 13:23:47 +0900

    User function code

--- src/main/java/server/user/dao/UserDao.java
+++ src/main/java/server/user/dao/UserDao.java
@@ -1,56 +1,13 @@
 package server.user.dao;
 
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import server.user.domain.User;
 
-import javax.sql.DataSource;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
-public class UserDao {
-    private DataSource dataSource;
-
-    public void setDataSource(DataSource dataSource) {
-        this.jdbcTemplate = new JdbcTemplate(dataSource);
-
-        this.dataSource = dataSource;
-    }
-
-    private JdbcTemplate jdbcTemplate;
-
-    private RowMapper<User> userMapper =
-            new RowMapper<User>() {
-                public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-                    User user = new User();
-                    user.setId(rs.getString("id"));
-                    user.setPassword(rs.getString("password"));
-                    user.setName(rs.getString("name"));
-                    user.setSex(rs.getString("sex"));
-                    user.setAge(rs.getInt("Age"));
-                    return user;
-                }
-            };
-
-    public void add(final User user) throws SQLException {
-        this.jdbcTemplate.update("insert into users(id, password, name, sex, age) values(?,?,?,?,?)",
-                user.getId(), user.getPassword(), user.getName(), user.getSex(), user.getAge());
-    }
-
-    public User get(String id) {
-        return this.jdbcTemplate.queryForObject("select * from users where id = ?", this.userMapper, new Object[] {id});
-    }
-
-    public void deleteAll() {
-        this.jdbcTemplate.update("delete from users");
-    }
-
-    public int getCount() throws SQLException  {
-        return this.jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
-    }
-
-    public List<User> getAll() {
-        return this.jdbcTemplate.query("select * from users order by id",this.userMapper);
-    }
+public interface UserDao {
+    void add(User user);
+    User get(String id);
+    List<User> getAll();
+    void deleteAll();
+    int getCount();
 }
