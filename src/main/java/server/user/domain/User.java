d4b1e236637a2bc94ec648bd05891e32f47eb00d
qhtkdrma <kimwonkyu15@gmail.com>
2022-02-17 12:54:13 +0900

    User setting code

--- src/main/java/server/user/domain/User.java
+++ src/main/java/server/user/domain/User.java
@@ -6,15 +6,31 @@ public class User {
     String name;
     String sex;
     int age;
+    Level level;
+    int visit;
+    int cash;
 
     public User() {    }
 
-    public User(String id, String password, String name, String sex, int age) {
+    public User(String id, String password, String name, String sex, int age, Level level, int visit, int cash) {
         this.id = id;
         this.password = password;
         this.name = name;
         this.sex = sex;
         this.age = age;
+        this.level = level;
+        this.visit = visit;
+        this.cash = cash;
+    }
+
+    public void upgradeLevel() {
+        Level nextLevel = this.level.nextLevel();
+        if (nextLevel == null) {
+            throw new IllegalStateException(this.level + "은 업그레이드가 불가능합니다.");
+        }
+        else {
+            this.level = nextLevel;
+        }
     }
 
     public String getId() {
@@ -55,5 +71,29 @@ public class User {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    public Level getLevel() {
+        return level;
+    }
+
+    public void setLevel(Level level) {
+        this.level = level;
+    }
+
+    public int getVisit() {
+        return visit;
+    }
+
+    public void setVisit(int visit) {
+        this.visit = visit;
+    }
+
+    public int getCash() {
+        return cash;
+    }
+
+    public void setCash(int cash) {
+        this.cash = cash;
     }
 }
