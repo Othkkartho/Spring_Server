23a761a39a764045b6dd53edc0655b70c20dffbb
qhtkdrma <kimwonkyu15@gmail.com>
2022-02-17 12:52:46 +0900

    User dao code

--- src/main/java/server/user/dao/UserDaoJdbc.java
+++ src/main/java/server/user/dao/UserDaoJdbc.java
@@ -2,6 +2,7 @@ package server.user.dao;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import server.user.domain.Level;
 import server.user.domain.User;
 
 import javax.sql.DataSource;
@@ -10,12 +11,8 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class UserDaoJdbc implements UserDao {
-    private DataSource dataSource;
-
     public void setDataSource(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
-
-        this.dataSource = dataSource;
     }
 
     private JdbcTemplate jdbcTemplate;
@@ -29,13 +26,16 @@ public class UserDaoJdbc implements UserDao {
                     user.setName(rs.getString("name"));
                     user.setSex(rs.getString("sex"));
                     user.setAge(rs.getInt("Age"));
+                    user.setLevel(Level.valueOf(rs.getInt("level")));
+                    user.setVisit(rs.getInt("visit"));
+                    user.setCash(rs.getInt("cash"));
                     return user;
                 }
             };
 
     public void add(final User user) {
-        this.jdbcTemplate.update("insert into users(id, password, name, sex, age) values(?,?,?,?,?)",
-                user.getId(), user.getPassword(), user.getName(), user.getSex(), user.getAge());
+        this.jdbcTemplate.update("insert into users(id, password, name, sex, age, level, visit, cash) values(?,?,?,?,?,?,?,?)",
+                user.getId(), user.getPassword(), user.getName(), user.getSex(), user.getAge(), user.getLevel(), user.getVisit(), user.getCash());
     }
 
     public User get(String id) {
@@ -52,5 +52,10 @@ public class UserDaoJdbc implements UserDao {
 
     public List<User> getAll() {
         return this.jdbcTemplate.query("select * from users order by id",this.userMapper);
+    }
+
+    public void update(User user) {
+        this.jdbcTemplate.update("update users set password = ?, name = ?, sex = ?, age = ?, level = ?, login = ?, visit = ?, cash = ?",
+                user.getPassword(), user.getName(), user.getSex(), user.getAge(), user.getLevel(), user.getVisit(), user.getCash());
     }
 }
