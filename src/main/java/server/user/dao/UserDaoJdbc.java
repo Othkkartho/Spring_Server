fad7a4f137dfc1662e2157f7f0b1983db0249ac7
qhtkdrma <kimwonkyu15@gmail.com>
2022-03-04 10:23:39 +0900

    User code File

--- src/main/java/server/user/dao/UserDaoJdbc.java
+++ src/main/java/server/user/dao/UserDaoJdbc.java
@@ -2,14 +2,18 @@ package server.user.dao;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
 import server.user.domain.Level;
 import server.user.domain.User;
+import server.user.sqlservice.SqlService;
 
 import javax.sql.DataSource;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
+@Repository
+@Service
 public class UserDaoJdbc implements UserDao {
     public void setDataSource(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
@@ -17,45 +21,49 @@ public class UserDaoJdbc implements UserDao {
 
     private JdbcTemplate jdbcTemplate;
 
+    private SqlService sqlService;
+
+    public void setSqlService(SqlService sqlService) {
+        this.sqlService = sqlService;
+    }
+
     private RowMapper<User> userMapper =
-            new RowMapper<User>() {
-                public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-                    User user = new User();
-                    user.setId(rs.getString("id"));
-                    user.setPassword(rs.getString("password"));
-                    user.setName(rs.getString("name"));
-                    user.setSex(rs.getString("sex"));
-                    user.setAge(rs.getInt("Age"));
-                    user.setLevel(Level.valueOf(rs.getInt("level")));
-                    user.setVisit(rs.getInt("visit"));
-                    user.setCash(rs.getInt("cash"));
-                    return user;
-                }
+            (rs, rowNum) -> {
+                User user = new User();
+                user.setId(rs.getString("id"));
+                user.setPassword(rs.getString("password"));
+                user.setName(rs.getString("name"));
+                user.setSex(rs.getString("sex"));
+                user.setAge(rs.getInt("Age"));
+                user.setLevel(Level.valueOf(rs.getInt("level")));
+                user.setVisit(rs.getInt("visit"));
+                user.setCash(rs.getInt("cash"));
+                return user;
             };
 
-    public void add(final User user) {
-        this.jdbcTemplate.update("insert into users(id, password, name, sex, age, level, visit, cash) values(?,?,?,?,?,?,?,?)",
+    public void add(User user) {
+        this.jdbcTemplate.update(this.sqlService.getSql("userAdd"),
                 user.getId(), user.getPassword(), user.getName(), user.getSex(), user.getAge(), user.getLevel().intValue(), user.getVisit(), user.getCash());
     }
 
     public User get(String id) {
-        return this.jdbcTemplate.queryForObject("select * from users where id = ?", this.userMapper, new Object[] {id});
+        return this.jdbcTemplate.queryForObject(this.sqlService.getSql("userGet"), this.userMapper, id);
     }
 
     public void deleteAll() {
-        this.jdbcTemplate.update("delete from users");
+        this.jdbcTemplate.update(this.sqlService.getSql("userDeleteAll"));
     }
 
     public int getCount()  {
-        return this.jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
+        return this.jdbcTemplate.queryForObject(this.sqlService.getSql("userGetCount"), Integer.class);
     }
 
     public List<User> getAll() {
-        return this.jdbcTemplate.query("select * from users order by id",this.userMapper);
+        return this.jdbcTemplate.query(this.sqlService.getSql("userGetAll"),this.userMapper);
     }
 
     public void update(User user) {
-        this.jdbcTemplate.update("update users set password = ?, name = ?, sex = ?, age = ?, level = ?, visit = ?, cash = ? where id = ?",
+        this.jdbcTemplate.update(this.sqlService.getSql(("userUpdate")),
                 user.getPassword(), user.getName(), user.getSex(), user.getAge(), user.getLevel().intValue(), user.getVisit(), user.getCash(), user.getId());
     }
 }
